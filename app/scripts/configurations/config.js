d70f12dffc7b18d7a0072261e175e7f017e855fa
Behrooz <behrooz.kamali@gmail.com>
2015-02-15 13:15:36 -0800

    add required high level requirements

---
+++ app/scripts/configurations/config.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name emergencyApp.CONFIG
+ * @description
+ * # CONFIG
+ * Constant in the emergencyApp.
+ */
+angular.module('emergencyApp')
+  .constant('CONFIG', 42);
---
+++ app/scripts/services/resource.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name emergencyApp.resource
+ * @description
+ * # resource
+ * Service in the emergencyApp.
+ */
+angular.module('emergencyApp')
+  .service('resource', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
