85dfb10926fc9fa6c6916d410f8055a930e7500d
Behrooz <behrooz.kamali@gmail.com>
2015-02-16 02:23:17 -0800

    remove temporary content, add angular-bootstrap, ui-router, and font awesome

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,33 +1,33 @@
 'use strict';
 
 /**
- * @ngdoc overview
- * @name emergencyApp
- * @description
- * # emergencyApp
- *
- * Main module of the application.
- */
+* @ngdoc overview
+* @name emergencyApp
+* @description
+* # emergencyApp
+*
+* Main module of the application.
+*/
 angular
-  .module('emergencyApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+.module('emergencyApp', [
+	'ngAnimate',
+	'ngCookies',
+	'ngResource',
+	'ngRoute',
+	'ngSanitize',
+	'ngTouch'
+	])
+.config(function ($routeProvider) {
+	$routeProvider
+	.when('/', {
+		templateUrl: 'views/main.html',
+		controller: 'MainCtrl'
+	})
+	.when('/about', {
+		templateUrl: 'views/about.html',
+		controller: 'AboutCtrl'
+	})
+	.otherwise({
+		redirectTo: '/'
+	});
+});
