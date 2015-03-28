193b4bab6f506e9aa78fa944e7b65bfa1cdee8ac
Behrooz <bkamali@marketshare.com>
2015-03-27 17:29:43 -0700

    implement resource service for rest api operations

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,22 +8,44 @@
  * Controller of the emergencyApp
  */
 angular.module('emergencyApp')
-  .controller('MainCtrl', function ($scope, $resource, $http, $cookies) {
-  	var req = {
-  		method: 'POST',
-  		url: 'http://localhost:8888/pronto/api/pronto/login',
-  		headers: {
-  			'AUTH_USER': 'behroozkamali@yahoo.com',
-  			'AUTH_PW': 'kamali'
-  		}
-  	};
-  	$http(req)
-  		.success(function(response) {
-	  		console.log(response);
-	  		console.log($cookies);
-	  	}).error(function() {
-	  		console.log(arguments);
-	  	});
-    // var user = $resource('http://localhost:8888/pronto/api/pronto/user/:userId', {userId: '@id'});
-    // console.log(user.get({userId: 20}));
-  });
+	.controller('MainCtrl', function ($scope, Resource, $http, $cookies) {
+		var headers = {
+				'AUTH_USER': 'behroozkamali@yahoo.com',
+				'AUTH_PW': 'kamali'
+			},
+			req = {
+				method: 'POST',
+				url: 'http://localhost:8080/pronto/api/pronto/login',
+				headers: headers
+			};
+
+		if(!!$cookies.uid && !!$cookies.ckey && !!$cookies.hkey) {
+			headers.UID = $cookies.uid;
+			headers.CKEY = $cookies.ckey;
+			headers.HKEY = $cookies.hkey;
+		}
+
+		var test = Resource('http://localhost:8080/pronto/api/pronto/user/20');
+		test.get().then(function(response) {
+			console.log(response);
+		}, function(error) {
+			console.log(error);
+		});
+
+		// $http(req)
+		// 	.success(function(response) {
+		// 		console.log(response);
+		// 		if(response.ckey && response.hkey && response.uid) {
+		// 			$cookies.ckey = response.ckey;
+		// 			$cookies.hkey = response.hkey;
+		// 			$cookies.uid = response.uid;
+		// 		}
+		// 		console.log($cookies);
+		// 	}).error(function(response, code) {
+		// 		if(code === 401) {
+		// 			delete $cookies.ckey// = response.ckey;
+		// 			delete $cookies.hkey// = response.hkey;
+		// 			delete $cookies.uid// = response.uid;
+		// 		}
+		// 	});
+	});
--- app/scripts/services/resource.js
+++ app/scripts/services/resource.js
@@ -1,13 +1,67 @@
 'use strict';
 
-/**
- * @ngdoc service
- * @name emergencyApp.resource
- * @description
- * # resource
- * Service in the emergencyApp.
- */
-angular.module('emergencyApp')
-  .service('resource', function () {
-    // AngularJS will instantiate a singleton by calling "new" on this function
-  });
+angular.module('emergencyApp').service('Resource', ['$http', '$q', function ($http, $q) {
+	var Resource = function(http, q, basePath) {
+		var getPath = function (additionalPath, routeParameters) {
+				var path = additionalPath ? basePath + '/' +  additionalPath : basePath;
+				path = routeParameters ? replaceRouteParameters(routeParameters, path) : path;
+				return path;
+			},
+			replaceRouteParameters = function(routeParameters, path) {
+				routeParameters.forEach(function(element, key){
+					var regExp = new RegExp(':' + key);
+					path = path.replace(regExp, element);
+				});
+				// remove unreplaced tokens
+				path = path.replace(/\/:[^\/]*/gi,'');
+
+				return path;
+			},
+			action = function(method, routeParameters, additionalPath, data, _config) {
+			var deferred = q.defer(),
+				config = _config || {};
+
+			if(method === 'POST' || method === 'PUT') {
+				if(typeof data === 'undefined') {
+					deferred.reject({status: 'No data provided'});
+					return deferred.promise;
+				} else {
+					config.data = data;
+				}
+			}
+
+			config.method = method;
+			config.url = getPath.call(this, additionalPath, routeParameters);
+			config.withCredentials = true;
+			config.headers = config.headers || {};
+			// config.headers.AUTH_USER = 'behroozkamali@yahoo.com';
+			// config.headers.AUTH_PW = 'kamali';
+
+			http(config)
+				.success(deferred.resolve)
+				.error(deferred.reject);
+
+			return deferred.promise;
+		};
+
+		this.get = function(additionalPath, routeParameters, config) {
+			return action('GET', routeParameters, additionalPath, undefined, config);
+		};
+
+		this.post = function(routeParameters, additionalPath, data, config) {
+			return action('POST', routeParameters, additionalPath, data, config);
+		};
+
+		this.put = function(routeParameters, additionalPath, data, config) {
+			return action('PUT', routeParameters, additionalPath, data, config);
+		};
+
+		this.delete = function(routeParameters, additionalPath, config) {
+			return action('DELETE', routeParameters, additionalPath, undefined, config);
+		};
+	};
+
+	return function(basePath) {
+		return new Resource($http, $q, basePath);
+	};
+}]);
