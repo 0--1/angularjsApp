7ccb16c944e330a27e4a9ea0793f8046989e93c1
Behrooz <behrooz.kamali@gmail.com>
2015-03-30 09:55:28 -0700

    add login view to the first page

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('myApp')
-	.controller('MainCtrl', ['$scope', 'UserModel', 'AuthModel', function ($scope, UserModel, AuthModel) {
+	.controller('LoginCtrl', function ($scope, SERVER, CONFIG, Resource, $http, $cookies, UserModel, AuthModel) {
 		AuthModel.login('behroozkamali@yahoo.com', 'kamali');
 		UserModel.getUser(20).then(function(user) {console.log(user);});
-	}]);
+	});
